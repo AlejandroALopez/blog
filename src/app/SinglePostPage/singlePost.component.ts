c18d32a902207e7e11a77b59a06f42dd942475fc
alejandro <lopez.alejandro@dali.dartmouth.edu>
2023-11-20 17:03:00 -0500

    image from s3 + code snippet working

--- src/app/SinglePostPage/singlePost.component.ts
+++ src/app/SinglePostPage/singlePost.component.ts
@@ -33,4 +33,13 @@ export class SinglePostComponent implements OnInit {
       );
     }
   }
+
+  // Method to get full AWS URL for an image
+  getImageUrl(imageId: string | undefined): string | undefined {
+    const postId = this.route.snapshot.paramMap.get('postId');
+    if(imageId) {
+      return `https://alopez-blog-images.s3.us-east-2.amazonaws.com/${postId}/${imageId}`;
+    }
+    return undefined;
+  }
 }
--- src/app/code-snippet/code-snippet.component.ts
+++ src/app/code-snippet/code-snippet.component.ts
@@ -6,25 +6,5 @@ import { Component, Input } from '@angular/core';
   styleUrl: './code-snippet.component.css',
 })
 export class CodeSnippetComponent {
-  code: string = `
-  import { HIGHLIGHT_OPTIONS } from 'ngx-highlightjs';
-
-  bootstrapApplication(AppComponent, {
-    providers: [
-      {
-        provide: HIGHLIGHT_OPTIONS,
-        useValue: {
-          coreLibraryLoader: () => import('highlight.js/lib/core'),
-          lineNumbersLoader: () => import('ngx-highlightjs/line-numbers'), // Optional, only if you want the line numbers
-          languages: {
-            typescript: () => import('highlight.js/lib/languages/typescript'),
-            css: () => import('highlight.js/lib/languages/css'),
-            xml: () => import('highlight.js/lib/languages/xml')
-          },
-          themePath: 'path-to-theme.css' // Optional, and useful if you want to change the theme dynamically
-        }
-      }
-    ]
-  })
-  `
+  @Input() code: string = "";
 }
